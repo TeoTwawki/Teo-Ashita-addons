79cbfd797de955c046d30157383512643103d678
TeoTwawki <arawn.annwn@yandex.com>
2022-08-17 18:11:37 -0400

    Add option to print flags to getinfo addon

--- v4/getinfo/getinfo.lua
+++ v4/getinfo/getinfo.lua
@@ -37,33 +37,49 @@ ashita.events.register('command', 'command_cb', function (e)
 
     if #args >= 1 and args[1]:ieq('/getinfo') then
         if #args == 3 and tonumber(args[3]) then
-            entity = GetEntity(bit.band(tonumber(args[3]), 0x0FFF))
+            if tonumber(args[3]) then
+                entity = GetEntity(bit.band(tonumber(args[3]), 0x0FFF))
+            end
+
+            if args[3] and not entity then
+                print(string.format("Error: entity %s not found", args[3]))
+                return
+            end
         end
 
-        if args[3] and not entity then
-            print(string.format("Error: entity %s not found", args[3])) 
-            return
-        end
-
-        if args[2]:any('?', 'help') or not entity then
+        if #args >= 2 and args[2]:any('?', 'help') or not entity then
             print("Usage:\n/getinfo optionalsubCommand optionalEntityID")
-            print("Subcommands:\nlook")
+            print("Subcommands:\nflags\nlook")
             return
         end
 
         print(string.format("Name: %s  ID: %d  Index: %d", entity.Name, target, index))
-        print(string.format("Speed: %d  Animation Speed  %d", entity.MovementSpeed * 10, entity.AnimationSpeed * 10))
-        if #args >= 2 and args[2]:any('look') then
-            print(string.format("Race ID: %i", entity.Race))
-            print(string.format("Hair model: %i", entity.Look.Hair))
-            print(string.format("Head model: %d", entity.Look.Head))
-            print(string.format("Body model: %d", entity.Look.Body))
-            print(string.format("Hand model: %d", entity.Look.Hands))
-            print(string.format("Legs model: %d", entity.Look.Legs))
-            print(string.format("Feet model: %d", entity.Look.Feet))
-            print(string.format("Mainhand model: %d", entity.Look.Main))
-            print(string.format("Offhand model ID: %d", entity.Look.Sub))
-            print(string.format("Ranged model ID: %d", entity.Look.Ranged))
+        print(string.format("Speed: %f  Animation Speed  %d", entity.MovementSpeed * 10, entity.AnimationSpeed * 10))
+        -- print(string.format("PosX: %f  PosZ: %f  PosY: %f", entity.X, entity.Y, entity.Z))
+
+        if #args >= 2 then
+            if args[2]:any('flags') then
+                print(string.format("Flags0: %d", entity.Render.Flags0)) -- Main Render Flags
+                print(string.format("Flags1: %d", entity.Render.Flags1)) -- Name Flags (Party, Away, Anon)
+                print(string.format("Flags2: %d", entity.Render.Flags2)) -- Name Flags (Bazaar, GM Icon, etc.)
+                print(string.format("Flags3: %d", entity.Render.Flags3)) -- Entity Flags (Shadow)
+                print(string.format("Flags4: %d", entity.Render.Flags4)) -- Name Flags (Name Visibility)
+                print(string.format("Flags5: %d", entity.Render.Flags5)) -- Geomancer Indi's
+                print(string.format("Flags6: %d", entity.Render.Flags6)) -- Unknown
+                print(string.format("Flags7: %d", entity.Render.Flags7)) -- Overhead Flags (Hi-word: Jump Emote, Model Visibility etc.) (Low-word: Job Mastery Stars, Party Seek Mastery Star, etc. [Low-Byte:] Timer of some sort.)
+                print(string.format("Flags8: %d", entity.Render.Flags8)) -- Overhead Flags (Job mastery party.)
+            elseif args[2]:any('look') then
+                print(string.format("Race ID: %i", entity.Race))
+                print(string.format("Hair model: %i", entity.Look.Hair))
+                print(string.format("Head model: %d", entity.Look.Head))
+                print(string.format("Body model: %d", entity.Look.Body))
+                print(string.format("Hand model: %d", entity.Look.Hands))
+                print(string.format("Legs model: %d", entity.Look.Legs))
+                print(string.format("Feet model: %d", entity.Look.Feet))
+                print(string.format("Mainhand model: %d", entity.Look.Main))
+                print(string.format("Offhand model ID: %d", entity.Look.Sub))
+                print(string.format("Ranged model ID: %d", entity.Look.Ranged))
+            end
         end
     end
 
